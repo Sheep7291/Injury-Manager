5772138b554e150dfc0e8d29ffd4ffd8d192e079
Sheep7291 <dominik02jankowski@wp.pl>
2025-06-07 16:21:47 +0200

    Initial Commit

---
+++ Injury-Manager/src/main/java/Training/Plan/Injury/Manager/InjuryManagerApplication.java
@@ -0,0 +1,13 @@
+package Training.Plan.Injury.Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjuryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjuryManagerApplication.class, args);
+	}
+
+}
---
+++ Injury-Manager/src/test/java/Training/Plan/Injury/Manager/InjuryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Training.Plan.Injury.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjuryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
